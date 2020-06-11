35e52a4c1ca0745ad6db67c0ceb609cb608b1921
o__n <58528833+shashank977@users.noreply.github.com>
2020-06-11 16:15:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ console.log(value);
 console.log(btns);
 
 btns.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        const styles = e.currentTarget.classList;
+    btn.addEventListener('click', function(each){
+        const styles = each.currentTarget.classList;
         if(styles.contains("decrease")){
             count--;
         }
@@ -19,7 +19,7 @@ btns.forEach(function(btn){
         }
         
         
-    /*-----color*-----*/
+    /*-----color-----*/
 
         if(count>0){
             value.style.color="green";
